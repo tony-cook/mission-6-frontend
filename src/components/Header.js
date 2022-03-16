49e81839a7c98e1f90acbfbfe9f1930c9e1249e9
Tony Cook <tony_cook@outlook.com>
2022-03-16 21:09:13 +1300

    header component completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Style.css';
 
 function Header() {
   return (
-    <div>Header</div>
+      <>
+        <div className='header-container'>
+            <div className='logo-image'>
+                <img className='logo-image' src='logo_large.jpg' alt='logo.png' />
+            </div>
+            <div className='navbar'>
+                <div className='navbar-button'>Property Management</div>
+                <div className='navbar-button'>Property Search</div>
+                <div className='navbar-button'>Body Corporate</div>
+                <div className='navbar-button'>Commercial</div>
+                <div className='navbar-button'>Waiheke Escapes</div>
+                <div className='navbar-button'>FAQ & Contact Us</div>
+            </div>
+
+        </div>
+
+    </>
   )
 }
 
